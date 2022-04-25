f95561092845de2c97bca71b4d734f07270d15ff
SebThiertant <sebastien.thiertant@gmail.com>
2022-04-25 16:42:06 +0200

    Second part of TP2 done. services.xml file is now added on the same location as main class.

--- src/main/java/com/thiertant/ecommerce/EcommerceApplication.java
+++ src/main/java/com/thiertant/ecommerce/EcommerceApplication.java
@@ -1,10 +1,13 @@
 package com.thiertant.ecommerce;
 
+import com.thiertant.ecommerce.service.ProductService;
 import model.Client;
 import model.Order;
 import model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -15,11 +18,22 @@ public class EcommerceApplication {
     public static void main(String[] args) {
         //SpringApplication.run(EcommerceApplication.class, args);
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("file:src/main/java/com/thiertant/ecommerce/services.xml");
+
+        ProductService productService = context.getBean("products", ProductService.class);
+
         Product product1 = new Product(1547234L, "Produit 1", "Ma description", 12.5, "image/ici", 150);
         Product product2 = new Product(1547227L, "Produit 2", "Ma description", 38.9, "image/ici", 150);
         Product product3 = new Product(122472L, "Produit 3", "Ma description", 150.5, "image/ici", 150);
         Product product4 = new Product(9805472L, "Produit 4", "Ma description", 15.0, "image/ici", 150);
 
+        productService.save(product1);
+        productService.save(product2);
+        productService.save(product3);
+        productService.save(product4);
+
+        productService.getAllProducts().stream().forEach(System.out::println);
+        /*
         Client client1 = new Client(18700980L, "Sébastien", "AHFIFZ");
         //System.out.println(client1);
 
@@ -38,6 +52,8 @@ public class EcommerceApplication {
         System.out.println(order);
         System.out.println(order.getTotalNumberOfProducts());
         System.out.println(order.getTotalOrderPrice());
+
+         */
     }
 
 }
